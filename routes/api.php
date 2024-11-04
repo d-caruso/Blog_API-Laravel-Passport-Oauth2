ee4305571f0c37317812e70efcfd2edadcc00447
d-caruso <caruso.domenico@gmail.com>
2024-11-04 18:47:26 +0100

    [TASK] PM-3 installed Passport; built migrations; added token routes; added oauth keys

---
+++ routes/api.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+$currentVersion = env('API_CURRENT_VERSION');
+
+Route::prefix("api/{$currentVersion}")->group(function () {
+
+    Route::post('/auth', 'AuthController@login');
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::middleware('throttle')->post('/token', [
+        'uses' => 'AccessTokenController@issueToken',
+        'as' => 'token',
+    ]);
+
+    Route::post('/token/refresh', [
+        'uses' => 'TransientTokenController@refresh',
+        'as' => 'token.refresh',
+    ]);
+
+    Route::post('/authorize', [
+        'uses' => 'ApproveAuthorizationController@approve',
+        'as' => 'authorizations.approve',
+    ]);
+
+    Route::delete('/authorize', [
+        'uses' => 'DenyAuthorizationController@deny',
+        'as' => 'authorizations.deny',
+    ]);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/tokens', [
+            'uses' => 'AuthorizedAccessTokenController@forUser',
+            'as' => 'tokens.index',
+        ]);
+
+        Route::delete('/tokens/{token_id}', [
+            'uses' => 'AuthorizedAccessTokenController@destroy',
+            'as' => 'tokens.destroy',
+        ]);
+
+        Route::get('/clients', [
+            'uses' => 'ClientController@forUser',
+            'as' => 'clients.index',
+        ]);
+
+        Route::post('/clients', [
+            'uses' => 'ClientController@store',
+            'as' => 'clients.store',
+        ]);
+
+        Route::put('/clients/{client_id}', [
+            'uses' => 'ClientController@update',
+            'as' => 'clients.update',
+        ]);
+
+        Route::delete('/clients/{client_id}', [
+            'uses' => 'ClientController@destroy',
+            'as' => 'clients.destroy',
+        ]);
+
+        Route::get('/scopes', [
+            'uses' => 'ScopeController@all',
+            'as' => 'scopes.index',
+        ]);
+
+        Route::get('/personal-access-tokens', [
+            'uses' => 'PersonalAccessTokenController@forUser',
+            'as' => 'personal.tokens.index',
+        ]);
+
+        Route::post('/personal-access-tokens', [
+            'uses' => 'PersonalAccessTokenController@store',
+            'as' => 'personal.tokens.store',
+        ]);
+
+        Route::delete('/personal-access-tokens/{token_id}', [
+            'uses' => 'PersonalAccessTokenController@destroy',
+            'as' => 'personal.tokens.destroy',
+        ]);
+    });
+});
